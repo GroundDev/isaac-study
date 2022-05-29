1faf18a868d50ee2a0af8ab03817991bf0103043
GroundDev <27danny@naver.com>
2022-05-29 18:58:53 +0900

    프로젝트생성

---
+++ EnglishMaster/EnglishMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnglishMaster
+//
+//  Created by KimJS on 2022/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnglishMaster/EnglishMaster/EnglishMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishMasterApp.swift
+//  EnglishMaster
+//
+//  Created by KimJS on 2022/05/29.
+//
+
+import SwiftUI
+
+@main
+struct EnglishMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
